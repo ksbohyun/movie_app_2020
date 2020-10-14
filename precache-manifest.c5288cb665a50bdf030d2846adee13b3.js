96f5c3ef5c4fdb2b8995da6e0ca77df56053f4b0
unknown <ksbohyun@naver.com>
2020-10-14 20:48:35 +0900

    Updates

---
+++ precache-manifest.c5288cb665a50bdf030d2846adee13b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f80d85cfa6d49a2a39706f5371e593",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "75eda4ec07cb9efea28a",
+    "url": "/movie_app_2020/static/css/main.ed92635c.chunk.css"
+  },
+  {
+    "revision": "a31f178eab92df145ae6",
+    "url": "/movie_app_2020/static/js/2.3db6c717.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3db6c717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75eda4ec07cb9efea28a",
+    "url": "/movie_app_2020/static/js/main.1bb35c00.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
